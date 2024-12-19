e17ba5fa185eeee0abb068693c653a5f33310c4d
Michael J. Wilson <mw9568@princeton.edu>
2024-12-19 13:59:53 -0500

    add example tests module

---
+++ src/tests.rs
@@ -0,0 +1,26 @@
+fn first_word(text: &str) -> &str {
+    match text.find(' ') {
+        Some(idx) => &text[..idx],
+        None => &text,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty() {
+        assert_eq!(first_word(""), "");
+    }
+
+    #[test]
+    fn test_single_word() {
+        assert_eq!(first_word("Hello"), "Hello");
+    }
+
+    #[test]
+    fn test_multiple_words() {
+        assert_eq!(first_word("Hello World"), "Hello");
+    }
+}
