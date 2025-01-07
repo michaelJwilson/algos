982111c29e9a954ca614266e7190dea2a7c934c3
Michael J. Wilson <mw9568@princeton.edu>
2025-01-07 11:20:23 -0500

    fix

--- src/fib.rs
+++ src/fib.rs
@@ -1,4 +1,8 @@
 fn fib(n: u32) -> u32 {
+    // NB next number is the sum of the previous two.
+    //    starts 0, 1, 1, 2, 3, 5, 8, ...
+    //
+    //    
     if n < 2 {
         return n;
     } else {
