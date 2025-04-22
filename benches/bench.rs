149d1395454ce68c943d9d092d47eeed5c700023
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-04-22 13:20:27 -0400

    fix

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,7 +2,30 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use petgraph::algo::ford_fulkerson as petgraph_ford_fulkerson;
 use algos::ford_fulkerson::get_large_graph_fixture;
 
+fn fibonacci(n: u64) -> u64 {
+    let mut a = 0;
+    let mut b = 1;
+
+    match n {
+        0 => b,
+
+        // NB all else.
+        _ => {
+            for _ in 0..n {
+                let c = a + b;
+
+                a = b;
+                b = c;
+            }
+
+            b
+        }
+    }
+}
+
 pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+    
     c.bench_function("petgraph_ford_fulkerson", |b| {
         b.iter(|| {
             // NB visium is 5K spots.
