976e463eabf79fe56c4908b7cdd244247b27c252
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-04-28 12:18:20 -0400

    fix

--- python/algos/rnn/embedding.py
+++ python/algos/rnn/embedding.py
@@ -9,20 +9,17 @@ logger = logging.getLogger(__name__)
 
 
 class GaussianEmbedding(nn.Module):
-    def __init__(self, device=None):
+    def __init__(self):
         super(GaussianEmbedding, self).__init__()
 
-        self.device = get_device(device)
+        self.device = get_device()
         self.num_states = Config().num_states
 
-        # NB Trainable parameters: mean and log(variance) for each state
-        # self.means = torch.randn(num_states, dtype=torch.float32).to(self.device)
-
         # NB fixed mean initialization.
-        self.means = torch.tensor([3.0, 8.0], dtype=torch.float32).to(self.device)
+        self.means = torch.tensor([3.0, 8.0], dtype=torch.float32, device=self.device)
 
         # NB fixed, unit variances.
-        self.log_vars = torch.zeros(self.num_states, dtype=torch.float32).to(self.device)
+        self.log_vars = torch.zeros(self.num_states, dtype=torch.float32, device=self.device)
 
         self.means = nn.Parameter(self.means, requires_grad=True)
         self.log_vars = nn.Parameter(self.log_vars, requires_grad=False)
@@ -46,14 +43,10 @@ class GaussianEmbedding(nn.Module):
         # NB log of normalization constant
         norm = 0.5 * torch.log(2. * torch.pi * variances_broadcast)
 
-        # TODO BUG device issue: self.means on cpu, not mps??
         # NB compute negative log-probabilities for each state and each value in the sequence
         neg_log_probs = (
             norm + ((x_broadcast - means_broadcast) ** 2) / variances_broadcast
         )
-
-        # TODO normalize with ~logsumexp.
-        # neg_log_probs = torch.log(F.softmin(neg_log_probs, dim=-1))
 
         # NB shape = (batch_size, sequence_length, num_states)
         return neg_log_probs
