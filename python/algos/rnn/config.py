cf34b38d0c368d76c9da8729299bb5ceef1eb8f3
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-04-28 10:58:21 -0400

    fix

--- python/algos/rnn/config.py
+++ python/algos/rnn/config.py
@@ -1,36 +1,35 @@
 import os
 import yaml
 from types import SimpleNamespace
+from pprint import pformat
+
 
 class Config:
     def __init__(self, config_path=None):
         config = self.load_config(config_path)
 
-        """
-        self.dataset = SimpleNamespace(
-            **{
-                "num_states": config.get("num_states"),
-                "num_sequences": config.get("num_sequences"),
-                "sequence_length": config.get("sequence_length"),
-            }
-        )
+        self.blocks = config.keys()
 
-        self.training = SimpleNamespace(
-            **{
-                "batch_size": config.get("batch_size"),
-                "num_layers": config.get("num_layers"),
-                "learning_rate": config.get("learning_rate"),
-                "num_epochs": config.get("num_epochs"),
-            }
-        )
-        """
-
-        self.dataset = SimpleNamespace(**config.get("dataset", {}))
-        self.training = SimpleNamespace(**config.get("training", {}))
+        for name in self.blocks:
+            setattr(self, name, SimpleNamespace(**config.get(name, {})))
 
     def __repr__(self):
-        return f"Config(dataset={self.dataset}, training={self.training})"
+        result = {}
 
+        for block in self.blocks:
+            result[block] = pformat(vars(getattr(self, block)), indent=4)
+
+        return (
+            f"Config(\n" + "\t".join(f"{k}:\n{v}" for k, v in result.items()) + "\n)\n"
+        )
+
+    def __getattr__(self, name):
+        for block in self.blocks:
+            if hasattr(getattr(self, block), name):
+                return getattr(getattr(self, block), name)
+
+        raise AttributeError(f"'Config' object has no attribute '{name}'")
+    
     def get_config_path(self):
         script_dir = os.path.dirname(os.path.abspath(__file__))
 
