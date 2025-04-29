4b248911492b818d02dafffc6e1d16d6240e1774
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-04-29 14:16:20 -0400

    fix

--- python/algos/rnn/utils.py
+++ python/algos/rnn/utils.py
@@ -9,26 +9,9 @@ from algos.rnn.config import Config
 logger = logging.getLogger(__name__)
 
 
-def get_device(device=None, index=0):
-    """
-    Returns a torch device according to the priority:
-      - keyword argument
-      - config definition, one of "native" or named,
-        e.g. cpu.
-    """
-    if device is None:
-        if (device := Config().device) == "native":
-            if torch.backends.mps.is_available():
-                # TODO HACK
-                device = "mps"
-            elif torch.cuda.is_available():
-                device = torch.cuda.current_device()
-            else:
-                device = "cpu"
-
-    logger.debug(f"Utilizing {device}:{index} device.")
-
-    return torch.device(f"{device}:{index}")
+def set_precision():
+    # NB mps has float only.
+    torch.set_default_dtype(torch.float32)
 
 
 def set_seed(seed):
@@ -39,14 +22,29 @@ def set_seed(seed):
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
-        
+
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
 
-def set_precision():
-    # TODO config defined.
-    torch.set_default_dtype(torch.float32)
+def get_device(device=None, index=0):
+    """
+    Returns a torch device according to the priority:
+      - keyword argument
+      - config definition, one of "native" or named,
+        e.g. cpu.
+    """
+    if (device is None) and (device := Config().device) == "native":
+        if torch.backends.mps.is_available():
+            device = "mps"
+        elif torch.cuda.is_available():
+            device = torch.cuda.current_device()
+        else:
+            device = "cpu"
+
+    logger.debug(f"Utilizing {device}:{index} device.")
+
+    return torch.device(f"{device}:{index}")
 
 
 def logmatexp(transfer, ln_probs):
