72e2ac33a35f4dc90173f09e34a5c50f0ff4342c
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-04-29 16:13:16 -0400

    fix

--- python/algos/rnn/hmm.py
+++ python/algos/rnn/hmm.py
@@ -28,6 +28,45 @@ class HMM(torch.nn.Module):
             ]
         )
 
+    def forward_backward(self, obvs):
+        # NB [batch_size, sequence_length, num_states]                                                                                                                                                     
+        ln_emission_probs = -self.layers[0].forward(obvs)
+
+        ln_fs = [interim := self.layers[1].forward(ln_emission_probs[:, 0, :])]
+
+        for ii in range(1, self.sequence_length):
+            ln_fs.append(
+                interim := ln_emission_probs[:, ii, :] + self.layers[2].forward(interim)
+            )
+
+        ln_fs = torch.stack(ln_fs, dim=1)
+
+        assert ln_fs.shape == ln_emission_probs.shape
+
+        # TODO Prince suggested no emission? confirm.                                                                                                                                                      
+        # NB https://pytorch.org/docs/stable/generated/torch.zeros_like.html                                                                                                                               
+        ln_bs = [interim := self.layers[1].forward(torch.zeros_like(ln_emission_probs[:, 0, :]))]
+
+        for ii in range(self.sequence_length - 2, -1, -1):
+            ln_bs.append(
+                interim := self.layers[2].forward(
+                    interim + ln_emission_probs[:, ii + 1, :]
+                )
+            )
+
+        ln_bs.reverse()
+
+        # NB https://pytorch.org/docs/stable/generated/torch.stack.html                                                                                                                                     
+        ln_bs = torch.stack(ln_bs, dim=1)
+
+        assert ln_bs.shape == ln_emission_probs.shape
+
+        ln_gamma = ln_fs + ln_bs
+
+        result = ln_gamma - torch.logsumexp(ln_gamma, dim=2, keepdim=True)
+
+        return result
+        
     # TODO return likelihood by forward only.
     def forward(self, obvs):
         # NB [batch_size, sequence_length, num_states]
@@ -44,39 +83,4 @@ class HMM(torch.nn.Module):
 
         assert ln_fs.shape == ln_emission_probs.shape
 
-        """
-        interim = self.layers[1].forward(ln_emission_probs[:, 0, :])
-
-        for ii in range(1, self.sequence_length):
-            interim += ln_emission_probs[:, ii, :] + self.layers[2].forward(interim)
-
-        return torch.sum(torch.logsumexp(interim, dim=-1))
-        """
-        # TODO P(x)
         return torch.sum(torch.logsumexp(ln_fs[:, -1, :], dim=-1))
-        """
-        # TODO Prince suggested no emission? confirm.
-        # NB https://pytorch.org/docs/stable/generated/torch.zeros_like.html
-        ln_bs = [interim := self.layers[1].forward(torch.zeros_like(ln_emission_probs[:, 0, :]))]
-
-        for ii in range(self.sequence_length - 2, -1, -1):
-            ln_bs.append(
-                interim := self.layers[2].forward(
-                    interim + ln_emission_probs[:, ii + 1, :]
-                )
-            )
-
-        ln_bs.reverse()
-            
-        # NB https://pytorch.org/docs/stable/generated/torch.stack.html
-        ln_bs = torch.stack(ln_bs, dim=1)
-
-        assert ln_bs.shape == ln_emission_probs.shape
-                
-        ln_gamma = ln_fs + ln_bs
-        
-        result = ln_gamma - torch.logsumexp(ln_gamma, dim=2, keepdim=True)
-
-        # NB -log marginal MAP on state.
-        return -result
-        """
