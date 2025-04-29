9fca2c452e8b06000db8e4596f65154e5056450d
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-04-29 14:42:25 -0400

    fix

--- python/algos/rnn/embedding.py
+++ python/algos/rnn/embedding.py
@@ -15,7 +15,7 @@ class GaussianEmbedding(nn.Module):
         super().__init__()
 
         config = Config()
-        
+
         self.device = get_device()
         self.num_states = config.num_states
 
@@ -24,6 +24,10 @@ class GaussianEmbedding(nn.Module):
 
         self.means = nn.Parameter(self.means, requires_grad=True)
         self.log_vars = nn.Parameter(self.log_vars, requires_grad=True)
+
+        assert (
+            len(self.means) == self.num_states
+        ), "Mean initialization provided inconsistent with number of states defined."
 
         logger.info(
             f"Initialized Gaussian embedding on device {self.device} with mean={self.means} (grad? {self.means.requires_grad}) and log vars={self.log_vars} (grad? {self.log_vars.requires_grad})"
@@ -40,20 +44,23 @@ class GaussianEmbedding(nn.Module):
         batch_size, sequence_length, _ = x.shape
         variances = torch.exp(self.log_vars)
 
-        # NB expand means and variances to match the sequence: (batch_size, 1, num_states); i.e. batch_size and sequence of 1.
-        means_broadcast = self.means.view(1, 1, self.num_states)
-        variances_broadcast = variances.view(1, 1, self.num_states)
+        # NB broadcast [num_states] to match: (batch_size, 1, num_states)
+        # means_broadcast = self.means.view(1, 1, self.num_states)
+        # variances_broadcast = variances.view(1, 1, self.num_states)
 
         # NB expand x to match the number of states: (batch_size, sequence_length, num_states)
         #    a view of original memory; -1 signifies no change;
-        x_broadcast = x.expand(-1, -1, self.num_states)
+        # x_broadcast = x.expand(-1, -1, self.num_states)
 
+        # NB no-copy view.
+        x = x.expand(-1, -1, self.num_states)
+        
         # NB log of normalization constant
-        norm = 0.5 * torch.log(2.0 * torch.pi * variances_broadcast)
+        norm = 0.5 * torch.log(2.0 * torch.pi * variances)
 
         # NB compute negative log-probabilities for each state and each value in the sequence
         neg_log_probs = (
-            norm + ((x_broadcast - means_broadcast) ** 2) / variances_broadcast
+            norm + ((x - self.means) ** 2) / norm
         )
 
         # NB shape = (batch_size, sequence_length, num_states)
