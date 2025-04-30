344773d51999bf62bfa7ba2fc44b0fd960ea2dbd
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-04-30 12:05:22 -0400

    fix

--- python/algos/tests/test_embedding.py
+++ python/algos/tests/test_embedding.py
@@ -1,57 +1,46 @@
 import pytest
 import torch
 from algos.rnn.embedding import GaussianEmbedding
+from algos.rnn.config import Config
 
 
 @pytest.fixture
-def gaussian_embedding():
+def config():
+    return Config()
+
+
+@pytest.fixture
+def normal_embedding():
     return GaussianEmbedding()
 
 
-def test_initialization(gaussian_embedding):
-    num_states = gaussian_embedding.num_states
+def test_initialization(normal_embedding):
+    num_states = normal_embedding.num_states
 
-    assert gaussian_embedding.means.shape == (num_states,), "Means shape is incorrect"
-    assert gaussian_embedding.log_vars.shape == (
+    assert normal_embedding.means.shape == (num_states,), "Means shape is incorrect"
+    assert normal_embedding.log_vars.shape == (
         num_states,
     ), "Log vars shape is incorrect"
-    assert gaussian_embedding.means.requires_grad, "Means should require gradients"
+    assert normal_embedding.means.requires_grad, "Means should require gradients"
     assert (
-        gaussian_embedding.log_vars.requires_grad
+        normal_embedding.log_vars.requires_grad
     ), "Log vars should not require gradients"
 
 
-def test_forward_pass(gaussian_embedding):
-    batch_size, sequence_length, num_features = 4, 10, 1
-    output = gaussian_embedding(
-        torch.randn(batch_size, sequence_length, num_features).to(
-            gaussian_embedding.device
+def test_embedding_forward(config, normal_embedding):
+    result = normal_embedding(
+        torch.randn(config.batch_size, config.sequence_length, 1).to(
+            normal_embedding.device
         )
     )
 
-    assert output.shape == (
-        batch_size,
-        sequence_length,
-        gaussian_embedding.num_states,
-    ), "Output shape is incorrect"
-
-
-def test_device_compatibility(gaussian_embedding):
-    assert (
-        gaussian_embedding.means.device == gaussian_embedding.device
-    ), f"Inconsistent means device for GaussianEmbedding {gaussian_embedding.means.device} and {gaussian_embedding.device}"
-
-    assert (
-        gaussian_embedding.log_vars.device == gaussian_embedding.device
-    ), "Log vars are not on the correct device"
-
-    batch_size, sequence_length, num_features = 4, 10, 1
-    output = gaussian_embedding(
-        torch.randn(batch_size, sequence_length, num_features).to(
-            gaussian_embedding.device
-        )
+    assert result.shape == (
+        config.batch_size,
+        config.sequence_length,
+        config.num_states,
     )
 
-    assert (
-        output.device == gaussian_embedding.device
-    ), "Output is not on the correct device"
+
+def test_embedding_device(normal_embedding):
+    assert normal_embedding.means.device == normal_embedding.device
+    assert normal_embedding.log_vars.device == normal_embedding.device
