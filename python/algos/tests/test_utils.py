e50b8a026dc1db5145e01e6d7980f8d09e9fb5db
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-04-30 13:59:38 -0400

    fix

--- python/algos/tests/test_utils.py
+++ python/algos/tests/test_utils.py
@@ -3,8 +3,8 @@ from algos.rnn.utils import get_device
 
 
 def test_get_device():
-    assert get_device("cpu") == torch.device("cpu", index=0)
+    assert get_device("cpu").type == torch.device("cpu", index=0).type
 
     # NB default to config defined "native"
-    assert (device := get_device()) == torch.device("mps:0")
+    assert (device := get_device()).type == torch.device("mps:0").type
 
