da431ab440a457b95c54f28978054324b8979c78
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-05-01 11:15:23 -0400

    fix

--- python/algos/rnn/hmm_dataset.py
+++ python/algos/rnn/hmm_dataset.py
@@ -3,9 +3,11 @@ import logging
 
 import numpy as np
 import torch
+from abc import ABC, abstractmethod
 from numba import njit
+from algos.rnn.config import Config
 from torch.utils.data import Dataset
-
+from scipy.stats import nbinom, beta
 from algos.rnn.utils import get_device, set_precision
 
 """
@@ -15,6 +17,20 @@ HMM simultation assuming Gaussian emission.
 logger = logging.getLogger(__name__)
 
 set_precision()
+
+
+class Emission(ABC):
+    """
+    Abstract base class for HMM emission models.
+    """
+
+    @abstractmethod
+    def load_parameters(self, config):
+        pass
+
+    @abstractmethod
+    def sample(self, states, sequence_length):
+        pass
 
 
 @njit
@@ -29,10 +45,48 @@ def populate_states(states, transfer):
         states[t] = np.searchsorted(sum_ps, sample)
 
 
-@njit
-def populate_obs_normal(obs, states, means, stds, sequence_length):
-    for t in range(sequence_length):
-        obs[t] = means[states[t]] + stds[states[t]] * np.random.randn()
+class NormalEmission(Emission):
+    def __init__(self):
+        config = Config()
+
+        self.means = np.array(config.gaussian_means)
+        self.stds = np.array(config.gaussian_stds)
+
+    def sample(self, state, sequence_length):
+        return self.means[state] + self.stds[state] * np.random.randn(sequence_length)
+
+    @njit
+    def populate_obvs(obvs, states, means, stds, sequence_length):
+        for t in range(sequence_length):
+            obvs[t] = means[states[t]] + stds[states[t]] * np.random.randn()
+
+
+class NBinomialEmission(Emission):
+    def __init__(self):
+        config = Config()
+
+        self.eff_coverages = np.array(config.nb_eff_coverages)
+        self.sampling = np.array(config.nb_sampling)
+
+    def populate_obvs(obvs, states, eff_coverages, samplings, sequence_length):
+        for t in range(sequence_length):
+            state = states[t]
+            obvs[t] = nbinom.rvs(eff_coverages[state], samplings[state])
+
+
+class BetaBinomEmission(Emission):
+    def __init__(self):
+        config = Config()
+
+        self.alphas = np.array(config.bb_alphas)
+        self.betas = np.array(config.bb_betas)
+
+    def populate_obvs(obvs, states, alphas, betas, sequence_length):
+        for t in range(sequence_length):
+            state = states[t]
+            pp = beta.rvs(alphas[state], betas[state])
+
+            obvs[t] = np.random.binomial(1, pp)
 
 
 class HMMDataset(Dataset):
@@ -41,8 +95,7 @@ class HMMDataset(Dataset):
         num_sequences,
         sequence_length,
         jump_rate,
-        means,
-        stds,
+        emission="normal",
         device=None,
         transform=None,
         target_transform=None,
@@ -94,10 +147,21 @@ class HMMDataset(Dataset):
 
         populate_states(self.states, self.trans)
 
+        match emission:
+            case "normal":
+                em = NormalEmission()
+                em.populate_obvs(self.obvs, self.states, self.means, self.stds, len(self.states))
+            case "nbinom":
+                em = NBinomialEmission()
+                em.populate_obvs(self.obvs, self.states, self.eff_coverages, self.sampling, len(self.states))
+            case "betabinom":
+                em = BetaBinomEmission()
+                em.populate_obvs(self.obvs, self.states, self.alphas, self.betas, len(self.states))
+        """
         populate_obs_normal(
             self.obvs, self.states, self.means, self.stds, self.sequence_length
         )
-
+        """
         if self.transform:
             self.obvs = self.transform(self.obvs)
 
@@ -107,6 +171,7 @@ class HMMDataset(Dataset):
         states = torch.tensor(
             self.states, dtype=torch.int, device="cpu", pin_memory=False
         )
+
         obvs = torch.tensor(self.obvs, device="cpu", pin_memory=False).unsqueeze(-1)
 
         logger.debug(f"{states}")
